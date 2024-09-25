10890dfd5cd20205023d28ed05cc20e3ab241484
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2024-09-25 20:14:07 +0530

    port updated

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(cookieParser())
 server.use(cors({
-    origin : "http://localhost:5175" ,
+    origin : "http://localhost:5173" ,
     credentials: true
 }))
 
