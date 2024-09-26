e43b9389d040f37d8bc732679f431c51cf36fdb6
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2024-09-26 15:19:15 +0530

    frontend url updated

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(cookieParser())
 server.use(cors({
-    origin : "http://localhost:5173" ,
-    credentials: true
+    origin : "https://justbuy001.netlify.app/login" ,
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
 server.use("/users", userRouter);
